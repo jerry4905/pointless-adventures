5784e7776e46d0963fd3831b984e55a1fc010991
Tima Nlemvo <tnlemvo@AML-TNLEMVO.caa.comgit commit -m added>
2021-05-29 21:35:43 -0700

    added Nav bar and footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+// Listener for Nav bar
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
 // save search results in local storage 
 
 // fetch information for apis
